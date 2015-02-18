3b54874c40e56e73bd1ab87d8db6438498fbf602
Sarah Joy Calpo & Christopher Joy-Webb <student@epicodus.com>
2015-02-18 14:29:43 -0800

    add basic functioning purchase ability

--- js/scripts.js
+++ js/scripts.js
@@ -29,3 +29,53 @@ var Ticket = {
      }
    }
 };
+
+
+$(function() {
+  var newTicket = Object.create(Ticket);
+
+  $("#movie-options h2").text(newTicket.movieName);
+
+  $('#Thursday').click(function() {
+    $('#movie1').text('Interstellar');
+    $('.times1 .showtime1').text('1600');
+    $('.times1 .showtime2').text('1900');
+    $('.times1 .showtime3').text('2130');
+
+    newTicket.day = 'Thursday';
+    $("#ticket-day").text(newTicket.day);
+
+    $('.times1 .showtime1').click(function () {
+      newTicket.movieName = $('#movie1').text();
+      newTicket.showtime = parseInt($(this).text());
+      $("#ticket-showtime").text(newTicket.showtime);
+      $("#ticket-movie").text(newTicket.movieName);
+    });
+
+    $('#calculate-price').submit(function(event) {
+      event.preventDefault();
+      // val inputtedYoung = parseInt($('#young').val());
+      // val inputtedOld = parseInt($('#old').val());
+      var inputtedAge = parseInt($('#input-age').val());
+
+      newTicket.age = inputtedAge;
+      newTicket.whatPrice();
+      var price = newTicket.price;
+
+      $('#final-price').text(price);
+    });
+
+  });
+
+  // $('#Wednesday').click(function() {
+  //   newTicket.movieName = 'Gone Girl';
+  //   $('#movie-options h2').text(newTicket.movieName);
+  //   $('.showtime1').text('3 pm');
+  // });
+  //
+  // $('#Friday').click(function() {
+  //   $('#movie-options h2').text(newTicket.movieName);
+  //   $('.showtime1').text('4 pm');
+  // });
+
+})
